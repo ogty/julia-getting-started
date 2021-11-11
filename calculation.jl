6dab41e1d2cfe4193816b8cda54f3873c6497502
ogty <mofuiti1803@gmail.com>
2021-11-11 10:50:26 +0900

    -

---
+++ calculation.jl
@@ -0,0 +1,34 @@
+x = +3
+y = -1
+t = true
+f = false
+
+println("""
+x = +3
+y = -1
+t = true
+f = false
+""")
+
+println("x + y  = $(x + y)")
+println("x - y  = $(x - y)")
+println("x * y  = $(x * y)")
+println("x / y  = $(x / y)")
+println("x ÷ y  = $(x ÷ y)")
+println("x \\ y  = $(x \ y)")
+println("x ^ y  = $(y ^ x)")
+println("x % y  = $(x % y)")
+println("x < y  = $(x < y)")
+println("x <= y = $(x <= y)") # ≤
+println("x > y  = $(x > y)") 
+println("x >= y = $(x >= y)") # x ≥ y
+println("!true  = $(!t)")
+println("-false = $(-f)")
+println("true & false   = $(t & f)")
+println("true | false   = $(t | f)")
+println("true ⊻ false   = $(t ⊻ f)")
+println("true >>> false = $(t >>> f)")
+println("true >> false  = $(t >> f)")
+println("true << false  = $(t << f)")
+println("true == flase  = $(t == f)")
+println("true != false  = $(t != f)") # x ≠ y
