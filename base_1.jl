66af5092a94d2b903e7728a1efd5c54b45d24666
ogty <mofuiti1803@gmail.com>
2021-11-12 16:07:51 +0900

    -

--- base_1.jl
+++ base_1.jl
@@ -78,10 +78,10 @@ nowmonth = Dates.format(today, "mm")
 function convabb1(month=nowmonth, etc...)
     result = []
     months = Dict{Int64, String}(
-    1  => "Jan", 2  => "Feb", 3  => "Mar",
-    4  => "Apr", 5  => "May", 6  => "Jun",
-    7  => "Jul", 8  => "Aug", 9  => "Sep",
-    10 => "Oct", 11 => "Nov", 12 => "Dec"
+        1  => "Jan", 2  => "Feb", 3  => "Mar",
+        4  => "Apr", 5  => "May", 6  => "Jun",
+        7  => "Jul", 8  => "Aug", 9  => "Sep",
+        10 => "Oct", 11 => "Nov", 12 => "Dec"
     )
 
     if typeof(month) == String
