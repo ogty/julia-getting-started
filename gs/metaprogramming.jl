bec298e06bd5cb3707617881ffa39dc5bc4f3059
ogty <mofuiti1803@gmail.com>
2021-11-14 00:35:34 +0900

    -

--- gs/metaprogramming.jl
+++ gs/metaprogramming.jl
@@ -22,3 +22,16 @@ end
 println(FLAG_A) # 1
 println(FLAG_B) # 2
 println(FLAG_C) # 4
+
+
+macro convabb(month)
+    months = Dict{Int64, String}(
+        1  => "Jan", 2  => "Feb", 3  => "Mar",
+        4  => "Apr", 5  => "May", 6  => "Jun",
+        7  => "Jul", 8  => "Aug", 9  => "Sep",
+        10 => "Oct", 11 => "Nov", 12 => "Dec"
+    )
+    :($(months[month]))
+end
+
+println(@convabb 11) # Nov
