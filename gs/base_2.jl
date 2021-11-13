d5c25451eadcef3a20e3ba35a9f2d54454edbbb1
ogty <mofuiti1803@gmail.com>
2021-11-13 21:03:43 +0900

    -

--- gs/base_2.jl
+++ gs/base_2.jl
@@ -1,3 +1,6 @@
+using Statistics: mean, std
+
+
 # list
 list = [1, 2]
 
@@ -34,3 +37,63 @@ println(s) # [2, 3, 1]
 println(union(s, [3, 4]))     # [4, 2, 3, 1]
 println(intersect(s, [1, 2])) # [2, 1]
 println(issubset([1, 3], s))  # true
+
+
+data1 = Array{Float64}(undef, 3, 2)
+data2 = zeros(Float64, 3, 2)
+data3 = ones(Float64, 3, 2)
+data4 = rand(Float64, 3, 2)
+data5 = randn(Float64, 3, 2)
+data6 = fill(1.1, 3, 2)
+
+println(data1)
+println(data2)
+println(data3)
+println(data4)
+println(data5)
+println(data6)
+
+copied = similar(data5, Float64)
+changed = reshape(data6, 2, 3)
+
+println(copied)
+println(changed)
+
+println(eltype(data1))
+println(length(data1))
+println(ndims(data1))
+println(size(data1))
+println(size(data1, 1))
+println(strides(data1))
+println(stride(data1, 2))
+
+A = collect(reshape(1:9, 3, 3))
+println(A)
+
+
+data7 = [1, 2, 3, 4, 5]
+println(mean(data7)) # 3.0
+println(std(data7))  # 1.5811388300841898
+# println(var(data7))  Error
+
+
+module MyCalc
+    export mysum
+
+    function mysum(data::Vector{Int64})
+        result = 0
+        for i in data
+            result += i
+        end
+        return result
+    end
+    function mymean(data::Vector{Int64})
+        return MyCalc.mysum(data) / length(data)
+    end
+end
+
+using .MyCalc
+
+println(mysum(data7))         # 15
+# println(mymean(data7))      Error
+println(MyCalc.mymean(data7)) # 3.0
---
+++ gs/call.jl
@@ -0,0 +1,55 @@
+module MySeries
+    export Calc
+    export Convabb
+    export convabb
+    
+
+    mutable struct Convabb{T}
+        data::T
+    end
+    function convabb(ca::Convabb{String})::Int64
+        months = Dict{String, Int64}(
+        "Jan" => 1,  "Feb" => 2,  "Mar" => 3,
+        "Apr" => 4,  "May" => 5,  "Jun" => 6,
+        "Jul" => 7,  "Aug" => 8,  "Sep" => 9,
+        "Oct" => 10, "Nov" => 11, "Dec" => 12
+        )
+    
+        return months[ca.data]
+    end
+    function convabb(ca::Convabb{Int64})::String
+        months = Dict{Int64, String}(
+            1  => "Jan", 2  => "Feb", 3  => "Mar",
+            4  => "Apr", 5  => "May", 6  => "Jun",
+            7  => "Jul", 8  => "Aug", 9  => "Sep",
+            10 => "Oct", 11 => "Nov", 12 => "Dec"
+        )
+        return months[ca.data]
+    end
+    
+    function convabb(ca::Convabb)
+        throw("Error")
+    end
+
+    module Calc
+        export mysum
+
+        function mysum(data::Vector{Int64})
+            result = 0
+            for i in data
+                result += i
+            end
+            return result
+        end
+        function mymean(data::Vector{Int64})
+            return MyCalc.mysum(data) / length(data)
+        end
+    end
+end
+
+
+using .MySeries
+
+ca = Convabb("Nov")
+println(convabb(ca))
+println(Calc.mysum([1, 2, 3, 4, 5]))
