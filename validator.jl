9173b8e7a3c8e31561618343eee53ca1585b0205
ogty <mofuiti1803@gmail.com>
2021-11-15 20:06:02 +0900

    -

--- validator.jl
+++ validator.jl
@@ -7,11 +7,22 @@ module Representation
     # Netowrks Model
     url = ""
     ipv4address = ""
+    ipv6address = ""
     mutable struct _Networks
         url::String
         ipv4address::String
+        ipv6address::String
 
-        _Networks(url="", ipv4address="") = new(url, ipv4address)
+        _Networks(
+            url="",
+            ipv4address="",
+            ipv6address=""
+            ) = begin new(
+                url, 
+                ipv4address,
+                ipv6address
+                )
+                end
     end
     mutable struct HttpUrl <: BaseModel
         network::_Networks
@@ -25,8 +36,18 @@ module Representation
         function IPv4Address(data, network::_Networks=_Networks(data))
             splited = split(data, ".")
             valid = [parse(Int64, b) for b in splited]
-            valid = [b for b in valid if b >= 0 & b<=255]
+            valid = [b for b in valid if b >= 0 & b <= 255]
             length(splited) == 4 & length(valid) == 4 || error(ValidationError)
+            new(network)
+        end
+    end
+    mutable struct IPv6Address <: BaseModel
+        network::_Networks
+        function IPv6Address(data, network::_Networks=_Networks(data))
+            """
+            
+            """
+            # error(ValidationError)
             new(network)
         end
     end
