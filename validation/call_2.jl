564017ad5b5298818bdd9940dbe5aec859343c09
ogty <mofuiti1803@gmail.com>
2021-11-25 02:16:12 +0900

    -

--- validation/call_2.jl
+++ validation/call_2.jl
@@ -2,20 +2,5 @@ include("./validator.jl")
 
 using .Representation
 
-ipaddresslist = ["192.168.10.1", "192.168.10.500", "0.0.0.0", "127.0.0.1"]
-validated = []
-
-for ipaddress in ipaddresslist
-    try
-        IPv4Address(ipaddress)
-        push!(validated, ipaddress)
-    catch e
-        println("Error: $ipaddress")
-    end
-end
-
-println(validated)
-
-# - Result - 
-# Error: 192.168.10.500
-# Any["192.168.10.1", "0.0.0.0", "127.0.0.1"]
+@IPv4Address "192.168.10.1"   # nothing
+@IPv4Address "192.168.10.500" # Error
