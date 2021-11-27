fcf799bd7b339ba06d191b8bdb98b97ac7aea2e1
ogty <mofuiti1803@gmail.com>
2021-11-27 21:05:56 +0900

    style: use unicode

--- bayesian_statistics/gradient.jl
+++ bayesian_statistics/gradient.jl
@@ -3,24 +3,24 @@ using PyPlot
 
 L = 10
 
-XS1 = range(-1, 1, length=L)
-XS2 = range(-1, 1, length=L)
+XS₁ = range(-1, 1, length=L)
+XS₂ = range(-1, 1, length=L)
 
-f(x) = -(x .+ 1)'*(x .- 1)
-deltaf(x) = -2x
+f₂(x) = -(x .+ 1)'*(x .- 1)
+∇f₂(x) = -2x
 
 fig, axes = subplots(1, 2, figsize=(8, 4))
 
-cs = axes[1].contour(XS1, XS2, [f([X1, X2]) for X1 in XS1, X2 in XS2])
+cs = axes[1].contour(XS₁, XS₂, [f₂([X₁, X₂]) for X₁ in XS₁, X₂ in XS₂])
 axes[1].clabel(cs, inline=true)
-axes[1].set_xlabel("X1"), axes[1].set_ylabel("X2")
-axes[1].set_title("f2(x)")
+axes[1].set_xlabel("X₁"), axes[1].set_ylabel("X₂")
+axes[1].set_title("f₂(x)")
 
-vec1 = [deltaf([X1, X2])[1] for X1 in XS1, X2 in XS2]
-vec2 = [deltaf([X1, X2])[2] for X1 in XS1, X2 in XS2]
-axes[2].quiver(repeat(XS1, 1, L), repeat(XS2', L, 1), vec1, vec2)
-axes[2].set_xlabel("X1"), axes[2].set_ylabel("X2")
-axes[2].set_title("deltaf(x)")
+vec1 = [∇f([X₁, X₂])[1] for X₁ in XS₁, X₂ in XS₂]
+vec2 = [∇f([X₁, X₂])[2] for X₁ in XS₁, X₂ in XS₂]
+axes[2].quiver(repeat(XS₁, 1, L), repeat(XS₂', L, 1), vec1, vec2)
+axes[2].set_xlabel("X₁"), axes[2].set_ylabel("X₂")
+axes[2].set_title("∇f₂(x)")
 
 tight_layout()
 savefig("../img/gradient.png")
