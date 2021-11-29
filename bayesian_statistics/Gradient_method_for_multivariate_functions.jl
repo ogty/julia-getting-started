be99c8883334066ec457842ecbd1423f2a809376
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2021-11-29 09:37:00 +0900

    Create Gradient_method_for_multivariate_functions.jl

---
+++ bayesian_statistics/Gradient_method_for_multivariate_functions.jl
@@ -0,0 +1,16 @@
+using PyPlot
+using Distributions
+
+
+x_opt = [0.50, 0.25]
+f₂(x) = -sqrt(0.05 + (x[1] - x_opt[1])^2) - (x[2] - x_opt[2])^2
+
+L = 100
+XS₁ = range(-1, 1, length=L)
+XS₂ = range(-1, 1, length=L)
+fig, ax = subplots()
+ax.contour(XS₁, XS₂, [f₂([X₁, X₂]) for X₁ in XS₁, X₂ in XS₂]')
+ax.plot(x_opt[1], x_opt[2], color="r", marker="x", label="optimal")
+ax.set_xlabel("x₁"), ax.set_ylabel("x₂")
+ax.grid()
+ax.legend()
