9d23bcca6b4f07b7e1c62528f44a79b2dc3dc6a1
Yoshinobu Ogata <92501820+ogty@users.noreply.github.com>
2021-11-29 09:35:33 +0900

    fix

--- bayesian_statistics/gradient.jl
+++ bayesian_statistics/gradient.jl
@@ -16,8 +16,8 @@ axes[1].clabel(cs, inline=true)
 axes[1].set_xlabel("X₁"), axes[1].set_ylabel("X₂")
 axes[1].set_title("f₂(x)")
 
-vec1 = [∇f([X₁, X₂])[1] for X₁ in XS₁, X₂ in XS₂]
-vec2 = [∇f([X₁, X₂])[2] for X₁ in XS₁, X₂ in XS₂]
+vec1 = [∇f₂([X₁, X₂])[1] for X₁ in XS₁, X₂ in XS₂]
+vec2 = [∇f₂([X₁, X₂])[2] for X₁ in XS₁, X₂ in XS₂]
 axes[2].quiver(repeat(XS₁, 1, L), repeat(XS₂', L, 1), vec1, vec2)
 axes[2].set_xlabel("X₁"), axes[2].set_ylabel("X₂")
 axes[2].set_title("∇f₂(x)")
